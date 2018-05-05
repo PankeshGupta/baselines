be44095e02324ac6c7655c9c82c20bb80decde5b
Sergey Kolesnikov <scitator@gmail.com>
2018-05-05 12:57:59 +0300

    hotfix

--- baselines/ppo2/policies.py
+++ baselines/ppo2/policies.py
@@ -36,7 +36,7 @@ def attn_cnn(unscaled_images, **conv_kwargs):
     h2 = h2 * conv_se(h2, 'se2')
     h3 = activ(conv(
         h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2), **conv_kwargs))
-    h3 = h3 * conv_se(h3, 'se2')
+    h3 = h3 * conv_se(h3, 'se3')
     h3 = conv_to_fc(h3)
     return activ(fc(h3, 'fc1', nh=512, init_scale=np.sqrt(2)))
 
